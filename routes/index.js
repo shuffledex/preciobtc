857984079f6a47f5e87ba9bc12888bf274847179
Federico Elgarte <federicoelgarte@LPITN200548.local>
2018-06-20 22:40:49 -0300

    logo, mail

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 var admin = require('firebase-admin');
 var serviceAccount = require(__base + 'preciobtc-firebase-adminsdk-nktyd-8c7f1e38eb.json');
@@ -21,6 +22,35 @@ router.get('/bitcoin-mejores-precios', function(req, res, next) {
 
 router.get('/bitcoin-calculadora', function(req, res, next) {
   res.render('bitcoin-calculadora', {});
+});
+
+router.get('/contacto', function(req, res, next) {
+  res.render('contacto', {});
+});
+
+router.post('/contacto', function(req, res, next) {
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'preciobtccom@gmail.com',
+      pass: 'bitcoinforever'
+    }
+  });
+
+  var mailOptions = {
+    from: req.body.email,
+    to: 'hola@preciobtc.com',
+    subject: req.body.razon,
+    text: req.body.mensaje
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      res.redirect('/contacto/?success=false');
+    } else {
+      res.redirect('/contacto/?success=true');
+    }
+  });
 });
 
 module.exports = function (io) {
